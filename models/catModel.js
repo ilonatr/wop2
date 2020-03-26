852e8c7b2add542b45bb82d32eaeadc4813ccbd0
ilonatr <ilona.tran@metropolia.fi>
2020-03-26 16:14:08 +0200

    Initial commit.

---
+++ models/catModel.js
@@ -0,0 +1,16 @@
+'use strict';
+const pool = require('../database/db');
+const promisePool = pool.promise();
+
+const getAllCats = async () => {
+  try {
+    const [rows] = await promisePool.query('SELECT * FROM wop_cat');
+    return rows;
+  } catch (e) {
+    console.log('error', e.message);
+  }
+};
+
+module.exports = {
+  getAllCats,
+};
